1c449b535b7eb8ce1ffc3581fbd4906b9c5a4107
Aitor Garzón <agarzon@ribesalat.com>
2021-04-02 19:30:10 +0200

    master Aplicar

--- src/js/card/card.mjs
+++ src/js/card/card.mjs
@@ -3,20 +3,19 @@ import fs from 'fs';
 
 // Constructor Card
 class Card {
-  constructor(number,data) {
+  constructor(number, data) {
     // Guardo el contenido del JSON en el Objeto Javascript
     this.type = data.type;
     this.name = data.name;
     this.nature = data.nature;
     this.vitality = data.vitality;
-	this.hability = data.hability;
-	this.strong = data.strong;
+    this.hability = data.hability;
+    this.strong = data.strong;
     this.attacks = data.attacks;
     this.weakness = data.weakness;
-	this.effect = data.effect;
+    this.effect = data.effect;
     this.retire = data.retire;
   }
-    
 }
 
 // Creo mapa de las Cards
@@ -24,8 +23,11 @@ let cards = new Map();
 
 // Busco todas las Cards
 for (let i = 1; i <= 25; i++) {
-  let card = new Card(i,JSON.parse(fs.readFileSync('database/card_' + i + '.json')));
-  cards.set('card_' + i,card);
+  let card = new Card(
+    i,
+    JSON.parse(fs.readFileSync('database/card_' + i + '.json')),
+  );
+  cards.set('card_' + i, card);
 }
 
 // Obtengo listado de todas las cards
--- src/js/hand.mjs
+++ src/js/hand.mjs
@@ -7,20 +7,29 @@ import fs from 'fs';
 // Constructor Hand
 class Hand {
   // Init Cards
-  Cards = [];
+  cards = [];
 
   constructor(cards_prototype) {
     // Obtengo JSON para mirar las cartas
     let hands_json = JSON.parse(fs.readFileSync('database/hand.json'));
 
     // Añado todas las Cards a mi array
-    for (let card = 1; card <= 25; card++) {
+    for (let card_count = 1; card_count <= 25; card_count++) {
       // Cantidad de Cartas
-      let cantidad = hands_json['card_' + card];
+      let cantidad = hands_json['card_' + card_count];
+      let total = 0;
 
+      // Repito las cartas según mazo
       for (let index = 1; index <= cantidad; index++) {
+        // Objeto Card
+        var card = {
+          name: cards_prototype.get('card_' + card_count).name,
+          
+          prototype: cards_prototype.get('card_' + card_count),
+        };
 
-        this.Cards.push(cards_prototype.get('card_' + card));
+        // Pongo Carta en Array
+        this.cards.push(card);
       }
     }
   }
