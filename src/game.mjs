3acc6cdf3a9dbc28f3c51066adef5ac3f02aca9a
Aitor Garzón <agarzon@ribesalat.com>
2021-04-15 13:46:18 +0200

    Refactoring

--- src/game.mjs
+++ src/game.mjs
@@ -14,16 +14,12 @@ class Game {
 	}
 
 	sendCommand(input, output) {
-
 		switch (input) {
-			case "start": this.initGame(input, output); this.draw(this.hands); break;
+			case "start": this.initGame(input, output); this.gui.draw(this.hands); break;
 			case "print": console.log(this.hands); break;
-			case "draw": this.draw(this.hands);
 			case "clear": console.clear(); break;
-			default: this.readGameState(input, output); this.readGameState("", output); this.draw(this.hands);
-
+			default: this.readGameState(input, output); this.readGameState("", output); this.gui.draw(this.hands);
 		}
-
 	}
 
 	// Give Cards to users again
@@ -73,87 +69,6 @@ class Game {
 						this.hands[1].turno(input, output, this.hands[0]);
 
 			}
-
-	}
-
-	// To Draw Cards to User
-	draw(hands) {
-
-		// Get Canvas
-		var canvas = document.getElementById("canvas");
-		canvas.width = (245 * 30) * 0.8;
-		canvas.height = ((342 * 2) * 0.8) + 90;
-		var ctx = canvas.getContext("2d");
-		let column = 0;
-		let row = 0;
-		let extra = 0;
-
-		// Font Size
-		ctx.font = "12px Arial Black";
-
-		// Get Hands
-		hands.forEach(
-			hand => {
-				// Get All Pokemon Cards in Hand
-				hand.cards.filter(card => (card.status === 'fight' || card.status === 'dock' || card.status === 'hand'))
-					.sort((a, b) => b.order() - a.order())
-					.forEach(card => {
-
-						// Let Informative text
-						let text = card.status;
-
-						// Get Image
-						var image = document.getElementById('img_' + card.image);
-
-						// Card Image
-						ctx.drawImage(image, (245 * 0.80) * (column), ((342 * 0.80) * row) + extra, (245 * 0.80), (342 * 0.80));
-
-						// Info Text
-						if (card.type === 'pokemon') {
-							// Add Info Text
-							text += " / " + card.vitality_now + "PS / ";
-
-							// Value Energy
-							let energy = "";
-
-							// Check Fire
-							if (card.energy.filter((v) => v === 'fire').length > 0) energy += 'Fire x' + card.energy.filter((v) => v === 'fire').length + " / ";
-
-							// Check Electric
-							if (card.energy.filter((v) => v === 'electric').length > 0) energy += 'Elec x' + card.energy.filter((v) => v === 'electric').length + " / ";
-
-							// Check Fight
-							if (card.energy.filter((v) => v === 'fight').length > 0) energy += 'Fight x' + card.energy.filter((v) => v === 'fight').length + " / ";
-
-							// Draw Text
-							ctx.fillText(energy.substring(0, energy.length - 2), (245 * 0.80) * (column), ((342 * 0.80) * (row + 1)) + extra + 30);
-
-						}
-
-						// Draw Text
-						ctx.fillText(text, (245 * 0.80) * (column), ((342 * 0.80) * (row + 1)) + extra + 13);
-
-
-						// Next Column
-						column++;
-
-					});
-
-				// Separación
-				ctx.beginPath();
-				ctx.moveTo(0, 315);
-				ctx.lineTo(canvas.width, 315);
-				ctx.stroke();
-
-				// Next Row
-				row++;
-				column = 0;
-				extra = 50;
-
-			}
-
-		);
-
 
 	}
 
--- src/gui.mjs
+++ src/gui.mjs
@@ -11,7 +11,7 @@ class GUI {
         // Canvas
         let canvas = document.createElement("canvas");
         canvas.id = "canvas";
-        canvas.width = 0; 
+        canvas.width = 0;
         canvas.height = 0;
         document.getElementsByTagName('body')[0].appendChild(canvas);
 
@@ -52,6 +52,87 @@ class GUI {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+
+    // To Draw Cards to User
+    draw(hands) {
+
+        // Get Canvas
+        var canvas = document.getElementById("canvas");
+        canvas.width = (245 * 30) * 0.8;
+        canvas.height = ((342 * 2) * 0.8) + 90;
+        var ctx = canvas.getContext("2d");
+        let column = 0;
+        let row = 0;
+        let extra = 0;
+
+        // Font Size
+        ctx.font = "12px Arial Black";
+
+        // Get Hands
+        hands.forEach(
+            hand => {
+                // Get All Pokemon Cards in Hand
+                hand.cards.filter(card => (card.status === 'fight' || card.status === 'dock' || card.status === 'hand'))
+                    .sort((a, b) => b.order() - a.order())
+                    .forEach(card => {
+
+                        // Let Informative text
+                        let text = card.status;
+
+                        // Get Image
+                        var image = document.getElementById('img_' + card.image);
+
+                        // Card Image
+                        ctx.drawImage(image, (245 * 0.80) * (column), ((342 * 0.80) * row) + extra, (245 * 0.80), (342 * 0.80));
+
+                        // Info Text
+                        if (card.type === 'pokemon') {
+                            // Add Info Text
+                            text += " / " + card.vitality_now + "PS / ";
+
+                            // Value Energy
+                            let energy = "";
+
+                            // Check Fire
+                            if (card.energy.filter((v) => v === 'fire').length > 0) energy += 'Fire x' + card.energy.filter((v) => v === 'fire').length + " / ";
+
+                            // Check Electric
+                            if (card.energy.filter((v) => v === 'electric').length > 0) energy += 'Elec x' + card.energy.filter((v) => v === 'electric').length + " / ";
+
+                            // Check Fight
+                            if (card.energy.filter((v) => v === 'fight').length > 0) energy += 'Fight x' + card.energy.filter((v) => v === 'fight').length + " / ";
+
+                            // Draw Text
+                            ctx.fillText(energy.substring(0, energy.length - 2), (245 * 0.80) * (column), ((342 * 0.80) * (row + 1)) + extra + 30);
+
+                        }
+
+                        // Draw Text
+                        ctx.fillText(text, (245 * 0.80) * (column), ((342 * 0.80) * (row + 1)) + extra + 13);
+
+
+                        // Next Column
+                        column++;
+
+                    });
+
+                // Separación
+                ctx.beginPath();
+                ctx.moveTo(0, 315);
+                ctx.lineTo(canvas.width, 315);
+                ctx.stroke();
+
+                // Next Row
+                row++;
+                column = 0;
+                extra = 50;
+
+            }
+
+        );
+
     }
 
 }
